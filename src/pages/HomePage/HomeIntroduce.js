9ecb038aefe9338da84ec5862b50a628f1951fea
annv3801 <annv3801@gmail.com>
2024-06-29 22:09:51 +0700

    update name

--- src/pages/HomePage/HomeIntroduce.js
+++ src/pages/HomePage/HomeIntroduce.js
@@ -5,7 +5,7 @@ export default function HomeIntroduce() {
                 <div className="heading text-center">
                     <h1 className="py-4 md:py-5 text-xl md:text-3xl font-bold uppercase text-yellow-600 inline-block relative bg-[#edf0f3] px-5 md:px-10 z-10">Rượu Duty Sân Bay - Mua bán thu mua rượu</h1>
                 </div>
-                <div className="text-center mx-5 pb-5">Shop Rượu – là đơn vị chuyên cung cấp rượu ngoại chính hãng, giá tốt tại Hà Nội. Được thành lập từ năm 1999, với hơn 20 năm kinh nghiệm chúng tôi luôn tự hào mang đến cho khách hàng từ trải nghiệm mua sắm đến chất lượng sản phẩm bậc nhất!</div>
+                <div className="text-center mx-5 pb-5">Rượu duty sân bay – là đơn vị chuyên cung cấp rượu ngoại chính hãng, giá tốt tại Hà Nội. Được thành lập từ năm 2010, với hơn 10 năm kinh nghiệm chúng tôi luôn tự hào mang đến cho khách hàng từ trải nghiệm mua sắm đến chất lượng sản phẩm bậc nhất!</div>
             </div>
         </div>
     );
